e1ce4f598640510237f29ecd5eaef5866694f969
pjh374968@gmail.com <pjh374968@gmail.com>
2019-01-17 15:57:09 +0900

    Add BaekJoon 1697 숨바꼭질

---
+++ BaekJoon/1697_숨바꼭질/Q1697.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Q1697 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int subinLocation = sc.nextInt();
+        int brotherLocation = sc.nextInt();
+        int MAX_NUM = 100001;
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] check = new boolean[MAX_NUM];
+        int[] dist = new int[MAX_NUM];
+
+        q.add(subinLocation);
+        dist[subinLocation] = 0;
+        check[subinLocation] = true;
+
+        int cnt = 0;
+
+        while (!q.isEmpty()) {
+            int n = q.poll();
+            cnt++;
+        
+            if (n-1 > 0 && check[n-1] == false) {
+                q.add(n-1);
+                check[n-1] = true;
+                dist[n-1] = dist[n] + 1;
+            }
+
+            if (n+1 < MAX_NUM && check[n+1] == false) {
+                q.add(n+1);
+                check[n+1] = true;
+                dist[n+1] = dist[n] + 1;
+            }
+
+            if (2*n < MAX_NUM && check[2*n] == false) {
+                q.add(2*n);
+                check[2*n] = true;
+                dist[2*n] = dist[n] + 1;
+            }
+        }
+        
+        System.out.println(dist[brotherLocation]);
+    }
+}
